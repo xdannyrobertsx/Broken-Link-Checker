882f03fa04f693f38e68bb974f2135c7c464d026
danny <xdannyrobertsx@gmail.com>
2023-08-29 23:24:27 -0500

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+# Link Checker Tool
+
+This is a simple Node.js script that checks the status of links in a list and generates a CSV report.
+
+## Requirements
+
+Ensure that you have the following software installed:
+
+- Node.js (v14 or later)
+- npm (Node Package Manager)
+
+## Getting Started
+
+1. Clone this repository.
+
+2. Place your list of links in a file named `links.txt` in the same directory as the script.
+
+3. Open a terminal and navigate to the project directory.
+
+4. Run the script:
+
+   ```sh
+   node checkLinks.js
+    ```
+
+5. Profit
---
+++ checkLinks.js
@@ -0,0 +1,77 @@
+import readline from 'readline';
+import { LinkChecker } from "linkinator";
+import fs from "fs/promises";
+import path from "path";
+import { SingleBar, Presets } from 'cli-progress';
+
+async function checkLinksFromFile(filename, filterBroken) {
+    const outputFilePath = 'broken_links.csv';
+
+    const currentWorkingDir = process.cwd();
+    const linksFilePath = path.join(currentWorkingDir, filename);
+
+    try {
+        // Check if the links.txt file exists
+        const fileExists = await fs.access(linksFilePath).then(() => true).catch(() => false);
+
+        if (!fileExists) {
+            console.error(`The file "${filename}" does not exist.`);
+            return;
+        }
+
+        const links = (await fs.readFile(linksFilePath, 'utf-8')).split('\n').filter(link => link.trim() !== '');
+
+        const checker = new LinkChecker();
+
+        const brokenLinks = [];
+
+        checker.on("link", (result) => {
+            if (!filterBroken || result.state === 'BROKEN') {
+                brokenLinks.push(result);
+            }
+        });
+
+        const progressBar = new SingleBar({
+            format: 'Progress |{bar}| {percentage}% || {value}/{total} Links',
+            barCompleteChar: '\u2588',
+            barIncompleteChar: '\u2591',
+            hideCursor: true,
+        }, Presets.shades_classic);
+
+        progressBar.start(links.length, 0);
+
+        for (const link of links) {
+            await checker.check({
+                path: `${link}`,
+                recurse: true
+            });
+
+            progressBar.increment();
+        }
+
+        progressBar.stop();
+
+        const csvContent = brokenLinks
+            .map((link) => `${link.url},${link.state === 'BROKEN' ? 'BROKEN' : 'OK'},${link.status},${link.parent}`)
+            .join("\n");
+        await fs.writeFile(outputFilePath, 'Link,Status,HTTP Code,Parent\n' + csvContent);
+
+        console.log(`Detected ${brokenLinks.length} broken links out of ${links.length} total links.`);
+    } catch (error) {
+        console.error('An error occurred:', error);
+    }
+}
+
+const linksFilename = 'links.txt'; // Change this to the desired filename
+
+// Prompt the user for filtering broken links
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Do you want to filter only broken links? (yes/no) [yes]: ', (answer) => {
+    const filterBroken = answer.trim().toLowerCase() === 'yes' || answer.trim() === '';
+    rl.close();
+    checkLinksFromFile(linksFilename, filterBroken);
+});
